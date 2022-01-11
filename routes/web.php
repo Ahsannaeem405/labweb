7467071978822fd1c9ba4a40a2eea1e19545cf94
Azeem918322 <81457043+Azeem918322@users.noreply.github.com>
2022-01-11 12:10:16 +0500

    cj

--- routes/web.php
+++ routes/web.php
@@ -153,6 +153,7 @@ Route:: prefix('/operator')->middleware(['auth', 'operator'])->group(function ()
 
 
 
+Route::post('/customer/details', [\App\Http\Controllers\CustomerController::class, 'detail_save']);
 
 
 
